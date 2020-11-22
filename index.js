9a7f3cad58eb26d33251cb76c4d820c6b8798ab7
Kamal Loudiyi <kaloudiyi@yahoo.fr>
2020-11-22 18:38:31 +0100

    update

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 class Concord {
   constructor() {
-    this.data = [];
+    this.data = []; // data
+    this.set = []; // set searched
+    this.sf = ''; // sort field
+    this.asc = true; // sort order
   }
 
   add(sentence) {
@@ -8,18 +11,53 @@ class Concord {
   }
 
   find(keyword) {
-    const res = [];
     const size = keyword.length;
+    this.set = [];
     this.data.forEach(sentence => {
       const rech = RegExp('\\b' + keyword + '\\b', 'gi').exec(sentence);
       if (rech)
-        res.push({
+        this.set.push({
           cg: sentence.substr(0, rech.index),
           kw: sentence.substr(rech.index, size),
           cd: sentence.substr(rech.index + size)
         });
     });
-    return res;
+  }
+
+  buildSortCd(lng = undefined) {
+    this.sf = 'cd';
+    this.asc = true;
+    this.set = this.set
+      .map(item => {
+        return {
+          sort: item.cd.split(/[ ,;:.?!]+/).join(' '),
+          ...item
+        };
+      })
+      .sort((a, b) => a.sort.localeCompare(b.sort));
+  }
+
+  buildSortCg(lng = undefined) {
+    this.sf = 'cg';
+    this.asc = true;
+    this.set = this.set
+      .map(item => {
+        return {
+          sort: item.cg
+            .split(/[ ,;:.?!]+/)
+            .reverse()
+            .join(' '),
+          ...item
+        };
+      })
+      .sort((a, b) => a.sort.localeCompare(b.sort));
+  }
+
+  toggleSort() {
+    this.asc = !this.asc;
+    this.set = this.set.sort((a, b) =>
+      this.asc ? a.sort.localeCompare(b.sort) : -a.sort.localeCompare(b.sort)
+    );
   }
 }
 
--- test.js
+++ test.js
@@ -16,9 +16,73 @@ describe('Concord', () => {
     concord.add('Can you translate this for me?');
     concord.add("I don't want to bother you.");
     concord.add("I'm very busy. I don't have time now.");
+    concord.add('You are welcome');
+    concord.find('you');
+    expect(concord.set.length).toEqual(3);
+    concord.find('trans');
+    expect(concord.set.length).toEqual(0);
+  });
 
-    console.log(concord.find('you'));
-    expect(concord.find('you').length).toEqual(2);
-    expect(concord.find('trans').length).toEqual(0);
+  test('Check the right context sort', () => {
+    const concord = new Concord();
+    concord.add('le chat joue a la pelote');
+    concord.add('le Chat joue à la balle');
+    concord.add('le chaton joue avec le stylo');
+    concord.add('le chaton joue, avec le crayon');
+    concord.find('joue');
+    expect(concord.set.length).toEqual(4);
+    concord.buildSortCd('fr');
+    expect(concord.set[0].cd).toEqual(' à la balle');
+    expect(concord.set[1].cd).toEqual(' a la pelote');
+    expect(concord.set[2].cd).toEqual(', avec le crayon');
+    expect(concord.set[3].cd).toEqual(' avec le stylo');
+  });
+
+  test('Check the left context sort', () => {
+    const concord = new Concord();
+    concord.add('le chat joue a la pelote');
+    concord.add('le chien joue a la pelote');
+    concord.add('la chatte joue avec un stylo');
+    concord.add('la chienne joue les os');
+    concord.find('joue');
+    expect(concord.set.length).toEqual(4);
+    concord.buildSortCg('fr');
+    expect(concord.set[0].cg).toEqual('le chat ');
+    expect(concord.set[1].cg).toEqual('la chatte ');
+    expect(concord.set[2].cg).toEqual('le chien ');
+    expect(concord.set[3].cg).toEqual('la chienne ');
+  });
+
+  test('Reverse the order to descent', () => {
+    const concord = new Concord();
+    concord.add('le chat joue a la pelote');
+    concord.add('le chien joue a la pelote');
+    concord.add('la chatte joue avec un stylo');
+    concord.add('la chienne joue les os');
+    concord.find('joue');
+    expect(concord.set.length).toEqual(4);
+    concord.buildSortCg('fr');
+    concord.toggleSort();
+
+    expect(concord.set[3].cg).toEqual('le chat ');
+    expect(concord.set[2].cg).toEqual('la chatte ');
+    expect(concord.set[1].cg).toEqual('le chien ');
+    expect(concord.set[0].cg).toEqual('la chienne ');
+  });
+
+  test('Reverse the order 2 times', () => {
+    const concord = new Concord();
+    concord.add('le chat joue a la pelote');
+    concord.add('le Chat joue à la balle');
+    concord.add('le chaton joue avec le stylo');
+    concord.add('le chaton joue, avec le crayon');
+    concord.find('joue');
+    concord.buildSortCd('fr');
+    concord.toggleSort();
+    concord.toggleSort();
+    expect(concord.set[0].cd).toEqual(' à la balle');
+    expect(concord.set[1].cd).toEqual(' a la pelote');
+    expect(concord.set[2].cd).toEqual(', avec le crayon');
+    expect(concord.set[3].cd).toEqual(' avec le stylo');
   });
 });
